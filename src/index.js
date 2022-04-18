a0bee5e30a19157b53fb21d712bcfbd5ea84772c
Gaurav Bhat <gaurav.bhat@klearnow.com>
2022-04-18 15:11:51 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
+      <Router>
     <App />
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
